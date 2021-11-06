49236ef9ec68032e6f015be991d431bb70fd78a6
gmdevito <73755122+gmdevito@users.noreply.github.com>
2021-11-06 15:24:51 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
 input.onPinPressed(TouchPin.P0, function () {
     radio.sendString("closed")
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(2000)
+    basic.clearScreen()
+})
 radio.setGroup(1)
